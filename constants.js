70062ada831c60033e1e5ac41b854bc43337e3f3
vinixhenri <vinixhenri@gmail.com>
2024-09-21 13:14:13 -0300

    Update constants.js

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     VENDOR_PDF_BOX_JAR: 'pdfbox-app-2.0.27.jar',
-    VENDOR_TIKA_JAR: 'tika-app-2.6.0.jar',
+    VENDOR_TIKA_JAR: 'tika-app-2.9.2.jar',
 
     DIRECTORY: {
         PDF: path.join(__dirname, './files/pdf/'),
